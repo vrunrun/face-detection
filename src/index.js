cd3a4ff035fb5931ba0728432e12e892d7b9979f
kl <kl@debian>
2019-12-05 11:25:41 +0800

    Add ProfileIcon component, an icon with dropdowns

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "tachyons";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
